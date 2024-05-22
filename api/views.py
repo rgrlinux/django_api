7814f54b6d91cd41dc7fca6515e7f676839933bb
rgrlinux <rgrlinux@gmail.com>
2024-05-22 20:59:11 -0300

    add views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from api.models import BlogPost
+from api.serializers import BlogPostSerializer
+
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+	queryset = BlogPost.objects.all()
+	serializer_class = BlogPostSerializer
+	
